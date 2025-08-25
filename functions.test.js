4d7b04326002107fef6a6db79fa1bd98244a6058
cdogcoder <cdogcoder@gmail.com>
2025-08-25 11:40:10 -0700

    Write first test for reverse string function

--- functions.test.js
+++ functions.test.js
@@ -3,19 +3,19 @@ import capitalize from "./functions.js";
 test("returns first letter capitalized with all lowercase letter string", () => {
   expect(capitalize("momma")).toBe("Momma");
 });
-
 test("returns first letter capitalized with all uppercase letter string", () => {
   expect(capitalize("MOMMA")).toBe("MOMMA");
 });
-
 test("returns empty string with empty string", () => {
     expect(capitalize("")).toBe("");
 })
-
 test("returns first letter capitalized with all mixed letter string", () => {
     expect(capitalize("hEllO")).toBe("HEllO");
 })
-
 test("returns first letter capitalized with random whitespace in front or behind string", () => {
     expect(capitalize("    heLlo  ")).toBe("HeLlo");
+})
+
+test("returns reversed string with just characters", () => {
+    expect(reverseString("hello")).toBe("olleh");
 })
