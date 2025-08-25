209db46fd37b35fcfd5090fed6b3e2df421e0489
cdogcoder <cdogcoder@gmail.com>
2025-08-25 11:20:38 -0700

    Write second test for capitalize function

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ import capitalize from "./functions.js";
 test("returns first letter capitalized with all lowercase letter string", () => {
   expect(capitalize("momma")).toBe("Momma");
 });
+
+test("returns first letter capitalized with all uppercase letter string", () => {
+    expect(capitalize("MOMMA").toBe("MOMMA"));
+})
