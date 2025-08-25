1a8d9730b072f5f4fec8376a5fa0fa412a245281
cdogcoder <cdogcoder@gmail.com>
2025-08-25 11:34:36 -0700

    Make fourth test for capitalize pass

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 export default function capitalize(word) {
   if (word.length) {
-    if (word !== "momma") {
-      return "MOMMA";
-    } else {
+    if (word === "momma") {
       return "Momma";
+    } else {
+        return word[0].toUpperCase() + word.slice(1);
     }
   } else {
     return "";
