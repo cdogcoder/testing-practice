09031db328b02af6719812061b3653c63d5ee22a
cdogcoder <cdogcoder@gmail.com>
2025-08-25 11:52:59 -0700

    Write fourth test for reverse string function

--- functions.test.js
+++ functions.test.js
@@ -25,3 +25,6 @@ test("returns reversed string with mixed characters", () => {
 test("returns reversed string with random whitespace in string", () => {
     expect(reverseString(" he ll   o")).toBe("olleh");
 })
+test("returns reversed string with numbers and symbols in string", () => {
+    expect(reverseString("  sa 112 1jd 2ej    ")).toBe("je2dj1211as")
+})
