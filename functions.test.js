0a48328a4888684925cc7a7219645b6da6eb84eb
cdogcoder <cdogcoder@gmail.com>
2025-08-26 09:14:23 -0700

    Write first test for analyze array function

--- functions.test.js
+++ functions.test.js
@@ -82,3 +82,12 @@ test("returns correct encryption for strings with uppercase letters", () => {
 test("returns correct encryption for strings with symbols", () => {
     expect(caesarCipher("Hello, World!",3)).toBe("Khoor, Zruog!");
 })
+
+test("returns object with correct details", () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toBe({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6,
+    })
+})
